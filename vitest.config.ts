948b4c3d0b276068b0dadbe308be40810387c78c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-22 07:21:44 +0300

    update workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,18 @@
 import { mergeConfig } from 'vite';
 import { configDefaults, defineConfig } from 'vitest/config';
+
 import viteConfig from './vite.config';
 
 export default mergeConfig(
+  // @ts-ignore
   viteConfig,
   defineConfig({
     test: {
       exclude: [...configDefaults.exclude, 'packages/template/*'],
-    },
-  }),
+      coverage: {
+        reporter: ['text', 'json-summary', 'json'],
+        provider: 'istanbul'
+      }
+    }
+  })
 );
